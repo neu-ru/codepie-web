e682339aa4b3501f827b05a3c3efdb560554cc19
DONXUX <ldu2175@gmail.com>
2022-09-01 23:24:38 +0900

    add: feed_thumbnail story

--- pages/feed_list/_feed_thumbnail.tsx
+++ pages/feed_list/_feed_thumbnail.tsx
@@ -3,18 +3,21 @@ import Avatar from "react-avatar";
 import Image from "next/image";
 import {getAssetURL} from "../../utils/asset";
 import Chip from "./_chip";
+import NumberUtils from "../../utils/number_utils";
+import Spacer from "../../component/style/spacer";
 
+// TODO: 유저 파리미터 추가
 export default function FeedThumbnail() {
     return (
         <Card>
             <Row>
-                <ProfileAndCodeContainer>
+                <Container>
                     <Row>
                         <Profile/>
-                        <Code/>
+                        <EditorThumbnail/>
                     </Row>
-                </ProfileAndCodeContainer>
-                <Buttons/>
+                </Container>
+                <Features/>
             </Row>
         </Card>
     );
@@ -24,20 +27,23 @@ function Profile() {
     return (
         <ProfileContainer>
             <Row>
+                <Avatar round="100px" size="36px"
+                        src={"https://scontent-nrt1-1.xx.fbcdn.net/v/t39.30808-1/" +
+                            "258867831_481563193336979_1631726358864355454_n.jpg?stp=dst-jpg_p480x480&_nc_cat=102&ccb=1-7" +
+                            "&_nc_sid=c6021c&_nc_ohc=RmAVRG8XacgAX94_ZNV&_nc_ht" +
+                            "=scontent-nrt1-1.xx&oh=00_AT-UhiZrDAfSfOTHhN0-dJ5w4T-wHaxdU40zvog5GXQ0Fg&oe=6312F3EE"}/>
                 <div>
-                    <Avatar round="100px" size="36px" src={""}/>
-                </div>
-                <div>
-                    <div>
-                        아아디
-                    </div>
-                    <div>
-                        8월 13일
-                    </div>
+                    <Name>
+                        이름
+                    </Name>
+                    <div/>
+                    <Date>
+                        8월 13일 오후 5:17
+                    </Date>
                 </div>
             </Row>
             <Row>
-                <Chip label={"Front-End"} selected={false} onClick={() => {
+                <Chip label={"Front-End"} selected={true} onClick={() => {
                 }}/>
                 <Chip label={"Back-End"} selected={true} onClick={() => {
                 }}/>
@@ -46,69 +52,78 @@ function Profile() {
     );
 }
 
-function Code() {
+// TODO: 에디터 컴포넌트 삽입
+function EditorThumbnail() {
     return (
-        <CodeContainer>
-            <div>
-                코드 제목
-            </div>
-            <div>
-                코드내용
-            </div>
-        </CodeContainer>
+        <EditorThumbnailContainer>
+            에디터
+        </EditorThumbnailContainer>
     );
 }
 
-function Buttons() {
+function Features() {
     return (
-        <ButtonsContainer>
-            <Column>
-                <Column>
-                    <Image
-                        loader={(props) => getAssetURL(props.src)}
-                        alt="ic_eye"
-                        width={24}
-                        height={24}
-                        src={'image/Eye.svg'}
-                    />
-                    <Text>20</Text>
-                </Column>
-                <Column>
-                    <Image
-                        loader={(props) => getAssetURL(props.src)}
-                        alt="ic_like"
-                        width={24}
-                        height={24}
-                        src={'image/Like.svg'}
-                    />
-                    20
-                </Column>
-                <Column>
-                    <Image
-                        loader={(props) => getAssetURL(props.src)}
-                        alt="ic_message"
-                        width={24}
-                        height={24}
-                        src={'image/Message.svg'}
-                    />
-                    20
-                </Column>
-                <Column>
-                    <Image
-                        loader={(props) => getAssetURL(props.src)}
-                        alt="ic_bookmark"
-                        width={24}
-                        height={24}
-                        src={'image/Bookmark.svg'}
-                    />
-                </Column>
-            </Column>
-        </ButtonsContainer>
+        <FeaturesContainer>
+            <Feature>
+                <Image
+                    loader={(props) => getAssetURL(props.src)}
+                    alt="ic_eye"
+                    width={24}
+                    height={24}
+                    src={'image/Eye.svg'}
+                />
+                <Text>{NumberUtils.format(543000)}</Text>
+            </Feature>
+            <Spacer height={'8px'}/>
+            <Feature>
+                <Image
+                    loader={(props) => getAssetURL(props.src)}
+                    alt="ic_like"
+                    width={24}
+                    height={24}
+                    src={'image/Like.svg'}
+                />
+                <Text>{NumberUtils.format(12300)}</Text>
+            </Feature>
+            <Spacer height={'8px'}/>
+            <Feature>
+                <Image
+                    loader={(props) => getAssetURL(props.src)}
+                    alt="ic_message"
+                    width={24}
+                    height={24}
+                    src={'image/Message.svg'}
+                />
+                <Text>{NumberUtils.format(349)}</Text>
+            </Feature>
+            <Spacer height={'35%'}/>
+            <Image
+                loader={(props) => getAssetURL(props.src)}
+                alt="ic_bookmark"
+                width={24}
+                height={24}
+                src={'image/Bookmark.svg'}
+            />
+        </FeaturesContainer>
     );
 }
 
-const Text = styled.div`
-  flex: 1;
+const Name = styled.text`
+  padding: 8px;
+`;
+
+const Date = styled.text`
+  padding: 8px;
+  font-size: 12px;
+  font-weight: lighter;
+  color: #A7A7A7;
+`;
+
+const Text = styled.text`
+  font-size: 12px;
+  margin-top: 2px;
+  font-weight: lighter;
+  text-align: center;
 `;
 
 const Card = styled.div`
@@ -121,17 +136,17 @@ const Card = styled.div`
 const Row = styled.div`
   width: 100%;
   display: flex;
+  margin-bottom: 8px;
   flex: 1;
   flex-direction: row;
 `;
 
-const Column = styled.div`
+const Feature = styled.div`
   display: flex;
-  flex: 1;
   flex-direction: column;
 `;
 
-const ProfileAndCodeContainer = styled.div`
+const Container = styled.div`
   flex: 1;
   padding: 32px;
   box-shadow: 1px 1px 8px #1B1B1B;
@@ -143,11 +158,12 @@ const ProfileContainer = styled.div`
   height: 100%;
 `;
 
-const ButtonsContainer = styled.div`
+const FeaturesContainer = styled.div`
   padding: 32px 16px;
   flex-direction: column;
 `;
 
-const CodeContainer = styled.div`
+const EditorThumbnailContainer = styled.div`
+  border: 1px solid yellow;
   flex: 3;
 `;
---
+++ stories/feed_thumbnail.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import FeedThumbnail from "../pages/feed_list/_feed_thumbnail";
+
+export default {
+    title: 'codepie/FeedThumbnail',
+    component: FeedThumbnail,
+} as ComponentMeta<typeof FeedThumbnail>;
+
+const Template: ComponentStory<typeof FeedThumbnail> = () => <FeedThumbnail/>;
+
+export const Basic = Template.bind({});
+Basic.args = {};
